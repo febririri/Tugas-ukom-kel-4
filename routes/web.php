746756b5536c844fffd799db3b89f40c583712ef
notrssa <carissa.lidra@smkn1kotabekasi.sch.id>
2025-11-15 11:47:11 +0700

    perubahanroute

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthController;
 
-<<<<<<< HEAD
-Route::get('/', function () {return view('index');});
-Route::get('/dashboard-guru', function () {return view('dashboard_guru');})->name('dashboard.guru');
-Route::get('/input-pelanggaran', function () {return view('input pelanggaran');})->name('input.pelanggaran');
-Route::get('/penghargaan', function () {
-    return view('penghargaan');
-})->name('input.penghargaan');
-Route::get('/sanksi', function () {
-    return view('sanksi');
-})->name('input.sanksi');
-=======
 // 🔹 Halaman utama
 Route::get('/', function () {
     return view('index');
@@ -40,4 +29,3 @@ Route::view('/sanksi', 'template.sanksi')->name('sanksi');
 
 // 🔹 Logout
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
->>>>>>> 12d03ddaef0fce47307ab939f9d24618db49470e
